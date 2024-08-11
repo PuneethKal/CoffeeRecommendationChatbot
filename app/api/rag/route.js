de7b2e718cda550b6b65ba0754685a4cebb65468
puneeth <puneeth.kallem@gmail.com>
2024-08-11 16:14:36 -0400

    RAG created using huggingface multilingual-e5-large(not connect to chatbot)

---
+++ app/api/rag/route.js
@@ -0,0 +1,52 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+import { NextResponse } from "next/server"
+import { HuggingFaceInferenceEmbeddings } from '@langchain/community/embeddings/hf';
+
+export async function POST(req) {
+    console.log('POST /api/rag')
+
+    let query = await req.json()
+    // console.log(typeof query)
+
+    // Initialize Huggingface Embedding model
+    const hf_embeddings = new HuggingFaceInferenceEmbeddings({
+        model: 'intfloat/multilingual-e5-large',
+        apiKey: process.env.HUGGINGFACE_API_KEY
+    })
+
+    // get embeddings for query
+    const query_embedding = await hf_embeddings.embedQuery(query)
+
+    // console.log(query_embedding)
+
+
+    // Initialize Pinecone
+    const pc = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY
+    });
+    const pinecone_index = pc.index('multilingualmodel');
+
+    // get top matches relating to query
+    const top_matches = await pinecone_index.namespace('youtube-videos-2').query({ vector: query_embedding, topK: 3, includeMetadata: true })
+
+    console.log(top_matches)
+
+    // Reterive context data from metadata
+    const contexts = top_matches.matches.map(item => item.metadata.text);
+
+    // console.log(contexts)
+
+    //Final query with the context from rag process
+    const augmented_query = `<CONTEXT>
+${contexts.slice(0, 10).join('\n\n-------\n\n')}
+-------
+</CONTEXT>
+
+
+MY QUESTION:
+${query}`;
+
+// console.log(augmented_query)
+
+    return new NextResponse(augmented_query)
+}
